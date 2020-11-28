8f1dd8caa65b163dadb8914ccb5e0a3b0f3dbe4d
Yunus Emre Inci <yemreinci0@gmail.com>
2020-11-28 23:29:11 +0300

    use float8 for mm5

--- mm/mm5.cpp
+++ mm/mm5.cpp
@@ -11,16 +11,16 @@ namespace cmpe492 {
 void
 mm(int n1, int n2, int n3, float const* mat1, float const* mat2, float* res)
 {
-    constexpr int nv = 4; // vector size
-    constexpr int nu = 2; // unrolling constant
+    constexpr int nv = 8; // vector size
+    constexpr int nu = 1; // unrolling constant
 
     const int n1r = (n1 + nu * nv - 1) / (nu * nv);
     const int n3r = (n3 + nu * nv - 1) / (nu * nv);
 
-    float4_t* const mat1_wrap =
-      static_cast<float4_t*>(aligned_alloc(sizeof(float4_t), n1r * n2 * nu * sizeof(float4_t)));
-    float4_t* const mat2_t_wrap =
-      static_cast<float4_t*>(aligned_alloc(sizeof(float4_t), n3r * n2 * nu * sizeof(float4_t)));
+    float8_t* const mat1_wrap =
+      static_cast<float8_t*>(aligned_alloc(sizeof(float8_t), n1r * n2 * nu * sizeof(float8_t)));
+    float8_t* const mat2_t_wrap =
+      static_cast<float8_t*>(aligned_alloc(sizeof(float8_t), n3r * n2 * nu * sizeof(float8_t)));
 
     assert(mat1_wrap);
     assert(mat2_t_wrap);
@@ -54,23 +54,29 @@ mm(int n1, int n2, int n3, float const* mat1, float const* mat2, float* res)
     auto sub_mm = [=](int from_row, int to_row) {
         for (int i = from_row; i < to_row; i++) {
             for (int j = 0; j < n3r; j++) {
-                float4_t t[nu][nu][nv] = {};
+                float8_t t[nu][nu][nv] = {};
 
                 for (int k = 0; k < n2; k++) {
                     for (int q1 = 0; q1 < nu; q1++) {
-                        float4_t v0_00 = mat1_wrap[(i * n2 * nu) + (k * nu) + (q1)];
-
-                        float4_t v0_01 = __builtin_shufflevector(v0_00, v0_00, 1, 0, 3, 2);
-                        float4_t v0_10 = __builtin_shufflevector(v0_00, v0_00, 2, 3, 0, 1);
-                        float4_t v0_11 = __builtin_shufflevector(v0_00, v0_00, 3, 2, 1, 0);
 
                         for (int q2 = 0; q2 < nu; q2++) {
-                            float4_t v1_00 = mat2_t_wrap[(j * n2 * nu) + (k * nu) + q2];
+                            float8_t v0_000 = mat1_wrap[(i * n2 * nu) + (k * nu) + (q1)];
+                            float8_t v1_000 = mat2_t_wrap[(j * n2 * nu) + (k * nu) + q2];
 
-                            t[q1][q2][0] += v0_00 * v1_00;
-                            t[q1][q2][1] += v0_01 * v1_00;
-                            t[q1][q2][2] += v0_10 * v1_00;
-                            t[q1][q2][3] += v0_11 * v1_00;
+                            float8_t v1_001 = __builtin_shufflevector(v1_000, v1_000, 1, 0, 3, 2, 5, 4, 7, 6);
+
+                            float8_t v0_100 = __builtin_shufflevector(v0_000, v0_000, 4, 5, 6, 7, 0, 1, 2, 3);
+                            float8_t v0_010 = __builtin_shufflevector(v0_000, v0_000, 2, 3, 0, 1, 6, 7, 4, 5);
+                            float8_t v0_110 = __builtin_shufflevector(v0_100, v0_100, 2, 3, 0, 1, 6, 7, 4, 5);
+
+                            t[q1][q2][0] += v0_000 * v1_000;
+                            t[q1][q2][1] += v0_000 * v1_001;
+                            t[q1][q2][2] += v0_010 * v1_000;
+                            t[q1][q2][3] += v0_010 * v1_001;
+                            t[q1][q2][4] += v0_100 * v1_000;
+                            t[q1][q2][5] += v0_100 * v1_001;
+                            t[q1][q2][6] += v0_110 * v1_000;
+                            t[q1][q2][7] += v0_110 * v1_001;
                         }
                     }
                 }
@@ -80,11 +86,11 @@ mm(int n1, int n2, int n3, float const* mat1, float const* mat2, float* res)
 
                         for (int w1 = 0; w1 < nv; w1++) {
                             for (int w2 = 0; w2 < nv; w2++) {
-                                int ri = i * (nu * nv) + q1 * nv + w1;
-                                int rj = j * (nu * nv) + q2 * nv + w2;
+                                int ri = i * (nu * nv) + q1 * nv + w2 ^ (w1 & 6);
+                                int rj = j * (nu * nv) + q2 * nv + w2 ^ (w1 & 1);
 
                                 if (ri < n1 && rj < n3) {
-                                    res[ri * n3 + rj] = t[q1][q2][w1^w2][w2];
+                                    res[ri * n3 + rj] = t[q1][q2][w1][w2];
                                 }
                             }
                         }
--- util/simd.hpp
+++ util/simd.hpp
@@ -1,5 +1,6 @@
 namespace cmpe492 {
 
 typedef float float4_t __attribute__((vector_size(4 * sizeof(float))));
+typedef float float8_t __attribute__((vector_size(8 * sizeof(float))));
 
 }
